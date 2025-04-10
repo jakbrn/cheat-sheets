3b38dc906ce3aa5427f80212f263603c6b77abc2
Jakub Baran <jakub.baran.b@gmail.com>
2025-04-10 13:06:44 +0200

    chore: update mailto link in SendMail component to use dynamic COOLIFY_FQDN

--- src/components/sheets/send-mail.tsx
+++ src/components/sheets/send-mail.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Button } from '../ui/button'
 import { Mail } from 'lucide-react'
 import { Lesson, Sheet } from '@/payload-types'
@@ -7,7 +5,7 @@ import { Lesson, Sheet } from '@/payload-types'
 export default function SendMail({ sheet }: { sheet: Sheet & { lesson: Lesson } }) {
   return (
     <a
-      href={`mailto:krzysztof@technischools.com?subject=%C5%9Aci%C4%85ga%20technologiczna%20-%20${sheet.lesson.name}&body=Poni%C5%BCej%20link%20oraz%20kod%20%C5%BAr%C3%B3d%C5%82owy%20%C5%9Bci%C4%85gi%20na%20sprawdzian%20z%20${sheet.lesson.inMail}%0D%0A%0D%0ALink%3A%20https%3A%2F%2Fsciaga.chub.pl%2Fsheets%2F${sheet.lesson.id}%0D%0AKod%20%C5%BAr%C3%B3d%C5%82owy%3A%20https%3A%2F%2Fgithub.com%2Fboxiercarpet%2Ftechni-cheat-sheets%0D%0A`}
+      href={`mailto:krzysztof@technischools.com?subject=%C5%9Aci%C4%85ga%20technologiczna%20-%20${sheet.lesson.name}&body=Poni%C5%BCej%20link%20oraz%20kod%20%C5%BAr%C3%B3d%C5%82owy%20%C5%9Bci%C4%85gi%20na%20sprawdzian%20z%20${sheet.lesson.inMail}%0D%0A%0D%0ALink%3A%20https%3A%2F%2F${process.env.COOLIFY_FQDN || 'localhost:3000'}%2Fsheets%2F${sheet.lesson.id}%0D%0AKod%20%C5%BAr%C3%B3d%C5%82owy%3A%20https%3A%2F%2Fgithub.com%2Fboxiercarpet%2Ftechni-cheat-sheets%0D%0A`}
     >
       <Button variant="outline" size="icon">
         <Mail size={20} />
